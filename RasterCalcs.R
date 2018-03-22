55e049f77004e0d3299d116f81f2ceb81237131b
smdevine <smdevine@ucdavis.edu>
2018-03-22 15:24:27 -0800

    have zonal summary of full regolith available water capacity for San Joaquin.  Still need to do Kings.

--- RasterCalcs.R
+++ RasterCalcs.R
@@ -6,7 +6,6 @@ library(raster)
 #convert from geographic to California Teale Albers
 # coords <- SpatialPoints(cbind(-2045640, 1740060), proj4string = crs("+proj=longlat +datum=NAD83 +no_defs +ellps=GRS80 +towgs84=0,0,0")) #this is Albers Equal Area coordinates
 # spTransform(coords, "+proj=longlat +datum=NAD83 +no_defs +ellps=GRS80 +towgs84=0,0,0")
-
 ResultsDir <- 'C:/Users/smdevine/Desktop/GITprojects/SouthernSierraAWS/results'
 SanJoaquinDir <- 'C:/Users/smdevine/Desktop/SpatialData/watershed_characteristics/san.joaquin'
 KingsDir <- 'C:/Users/smdevine/Desktop/SpatialData/watershed_characteristics/uf_kings_stats'
@@ -16,6 +15,7 @@ elev.zones <- DEM.san.joaquin
 elev.zones[elev.zones < 600] <- 1
 elev.zones[elev.zones >= 600 & elev.zones <= 2000] <- 2
 elev.zones[elev.zones > 2000] <- 3
+names(elev.zones) <- 'elev.zones'
 elev.zone.summary <- freq(elev.zones)
 elev.zone.summary <- as.data.frame(elev.zone.summary)
 elev.zone.summary <- elev.zone.summary[-4,]
@@ -82,7 +82,7 @@ kings.stack <- stack(elev.zones.kings, aws.kings)
 names(kings.stack) <- c('zones', 'aws_mu_wtdavg')
 zonal.kings.aws$NA.count[1] <- cellStats(kings.stack$zones==1 & is.na(kings.stack$aws_mu_wtdavg), 'sum')
 zonal.kings.aws$NA.count[2] <- cellStats(kings.stack$zones==2 & is.na(kings.stack$aws_mu_wtdavg), 'sum')
-zonal.kings.aws$NA.count[3] <- cellStats(kings.stack$zones==3 & is.na(kings.stack$aws_mu_wtdavg), 'sum') 
+zonal.kings.aws$NA.count[3] <- cellStats(kings.stack$zones==3 & is.na(kings.stack$aws_mu_wtdavg), 'sum')
 zonal.kings.aws$NA.AWS.hectares <- zonal.kings.aws$NA.count*900/10000
 #total NA count needs to be confirmed
 zonal.kings.aws$hectares <- elev.zone.summary.kings$hectares
@@ -109,3 +109,51 @@ zonal.kings.aws$elevation <- zonal(kings.stack$DEM, kings.stack$zones, fun='mean
 zonal.kings.aws
 write.csv(zonal.kings.aws, file.path(ResultsDir, 'zonal.summary.kings.csv'), row.names = FALSE)
 sum(zonal.kings.aws$no.id.deephorizon.hectares)
+
+#subsitute mukey raster with new major component only and zonal soils data
+mu_results_all <- read.csv(file.path(ResultsDir, 'mu_aggregated_san.joaquin.3.22.18.csv'), stringsAsFactors = FALSE)
+mukey_raster_san.joaquin <- raster(file.path(SanJoaquinDir, 'soil_mukey.tif'))
+#names(mukey_raster_san.joaquin) <- 'mukey'
+head(mu_results_all)
+sum(mu_results_all$hectares)
+aws_results_all <- mu_results_all[ ,c('mukey', "awc_ssurgo_majcomps", 'awc_zone1', 'awc_zone2', 'awc_zone3')] #by column needs to be number 1 for some reason for subs to work
+subs(mukey_raster_san.joaquin, aws_results_all, by='mukey', which='awc_ssurgo_majcomps', filename=file.path(SanJoaquinDir, 'aws_majcomps_only.tif'), format='GTiff', overwrite=TRUE)
+
+#now make a stack to create full regolith estimate
+full_regolith_stack <- stack(mukey_raster_san.joaquin, elev.zones)
+full_regolith_stack$aws_full_regolith <- full_regolith_stack$elev.zones #to be replaced below
+full_regolith_stack$zone1_assumption <- subs(full_regolith_stack$soil_mukey, aws_results_all, by='mukey', which='awc_zone1', filename=file.path(SanJoaquinDir, 'zone1_assumption.tif'), format='GTiff', overwrite=TRUE)
+full_regolith_stack$zone2_assumption <- subs(full_regolith_stack$soil_mukey, aws_results_all, by='mukey', which='awc_zone2', filename=file.path(SanJoaquinDir, 'zone2_assumption.tif'), format='GTiff', overwrite=TRUE)
+full_regolith_stack$zone3_assumption <- subs(full_regolith_stack$soil_mukey, aws_results_all, by='mukey', which='awc_zone3', filename=file.path(SanJoaquinDir, 'zone3_assumption.tif'), format='GTiff', overwrite=TRUE)
+full_regolith_stack$aws_full_regolith[full_regolith_stack$elev.zones==1] <- full_regolith_stack$zone1_assumption[full_regolith_stack$elev.zones==1]
+full_regolith_stack$aws_full_regolith[full_regolith_stack$elev.zones==2] <- full_regolith_stack$zone2_assumption[full_regolith_stack$elev.zones==2]
+full_regolith_stack$aws_full_regolith[full_regolith_stack$elev.zones==3] <- full_regolith_stack$zone3_assumption[full_regolith_stack$elev.zones==3]
+writeRaster(full_regolith_stack$aws_full_regolith, filename = file.path(SanJoaquinDir, 'aws_full_regolith.tif'), format = 'GTiff', overwrite=TRUE)
+full_regolith_stack$aws_majcomps_only <- raster(file.path(SanJoaquinDir, 'aws_majcomps_only.tif'))
+full_regolith_stack$aws_0150 <- raster(file.path(SanJoaquinDir, 'aws0150wta.tif'))
+full_regolith_stack$precip.annual <- raster(file.path(SanJoaquinDir, 'ppt1981_2010avg.tif'))
+full_regolith_stack$aprsnpck.annual <- raster(file.path(SanJoaquinDir, 'aprpck1981_2010avg.tif'))
+full_regolith_stack$slope <- raster(file.path(SanJoaquinDir, 'slope.tif'))
+full_regolith_stack$pet.annual <- raster(file.path(SanJoaquinDir, 'pet1981_2010avg.tif'))
+
+#1 km3 is 810,714 acre feet
+cellStats(full_regolith_stack$aws_full_regolith, stat='mean') #13.14 cm
+cellStats(full_regolith_stack$aws_full_regolith/100 * 900 / 1000^3, 'sum') #0.5518573 km3 or 447,398.7 acre feet
+cellStats(!is.na(full_regolith_stack$aws_full_regolith), 'sum') #4667392 which is 420,065.3 ha
+(13.14/100 * 900 * 4667392) / 1000^3 #check answer above
+cellStats(full_regolith_stack$aws_majcomps_only, stat = 'mean') #7.36 cm
+cellStats(full_regolith_stack$aws_majcomps_only/100 * 900 / 1000^3, 'sum') #0.3093358 or 250,782.9 acre-feet
+cellStats(full_regolith_stack$aws_0150, stat = 'mean') #7.36 cm
+cellStats(full_regolith_stack$aws_0150/100 * 900 / 1000^3, 'sum') #0.3592872 or 291,279.2 acre-feet
+
+#add these back to the zonal summary table
+#use this as template: zonal(kings.stack$aws0150.kings, kings.stack$zones, fun = 'mean')[,2]
+elev.zone.summary.san.joaquin <- read.csv(file.path(ResultsDir, 'zonal.summary.san.joaquin.csv'), stringsAsFactors = FALSE)
+elev.zone.summary.san.joaquin$aws_majcomps_SSURGOmodified <- zonal(full_regolith_stack$aws_majcomps_only, full_regolith_stack$elev.zones, fun='mean')[,2]
+elev.zone.summary.san.joaquin$aws_full_regolith <- zonal(full_regolith_stack$aws_full_regolith, full_regolith_stack$elev.zones, fun='mean')[,2]
+elev.zone.summary.san.joaquin$precip.annual <- zonal(full_regolith_stack$precip.annual, full_regolith_stack$elev.zones, fun='mean')[,2]
+elev.zone.summary.san.joaquin$aprsnpck.annual <- zonal(full_regolith_stack$aprsnpck.annual, full_regolith_stack$elev.zones, fun='mean')[,2]
+elev.zone.summary.san.joaquin$pet.annual <- zonal(full_regolith_stack$pet.annual, full_regolith_stack$elev.zones, fun='mean')[,2]
+elev.zone.summary.san.joaquin$slope <- zonal(full_regolith_stack$slope, full_regolith_stack$elev.zones, fun='mean')[,2]
+write.csv(elev.zone.summary.san.joaquin, file.path(ResultsDir, 'zonal.summary.san.joaquin.v2.csv'), row.names = FALSE)
+
